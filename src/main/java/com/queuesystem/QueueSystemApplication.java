4d547f52ccfae7fac4607ab792f39c3b86c8895f
GrzeluD <grzeluud@gmail.com>
2024-02-14 17:45:29 +0100

    fixed SecurityFilterChain in WebSecurityConfig

--- src/main/java/com/queuesystem/QueueSystemApplication.java
+++ src/main/java/com/queuesystem/QueueSystemApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class QueueSystemApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(QueueSystemApplication.class, args);
 	}
-
 }
--- src/main/java/com/queuesystem/security/config/WebSecurityConfig.java
+++ src/main/java/com/queuesystem/security/config/WebSecurityConfig.java
@@ -6,18 +6,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @AllArgsConstructor
@@ -32,7 +28,7 @@ public class WebSecurityConfig {
                 csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests((requests) -> requests.requestMatchers(HttpMethod.POST,
                                 "/api/v1/registration/**")
-                .permitAll().anyRequest().authenticated());
+                .permitAll().anyRequest().authenticated()).authenticationProvider(authenticationProvider()).formLogin(Customizer.withDefaults());
 
         return http.build();
     }
